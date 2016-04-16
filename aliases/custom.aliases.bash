2db1b8954008293d094c9edb38dae115fc51bcd0
xaos52 <xaos52@gmail.com>
2016-04-16 11:25:23 +0200

    Add custom aliases.

---
+++ aliases/custom.aliases.bash
@@ -0,0 +1,9 @@
+alias rm='rm -i'
+alias dt='/opt/darktable/bin/darktable'
+alias acs='apt-cache search'
+alias acsh='apt-cache show'
+alias upgr='sudo apt-get update && sudo apt-get -V upgrade'
+alias dpl='dpkg -l'
+alias dpL='dpkg -L'
+
+export EDITOR='emacsclient -c'
