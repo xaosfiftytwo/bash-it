ec0ca09a010d5d29392efb0eeb43af89ca99a8d9
xaos52 <xaos52@gmail.com>
2016-09-05 10:36:10 +0200

    Added custom aliases

--- aliases/custom.aliases.bash
+++ aliases/custom.aliases.bash
@@ -1,10 +1,12 @@
 alias rm='rm -i'
 alias dt='/opt/darktable/bin/darktable'
-alias acp='ap-cache policy'
+alias acp='apt-cache policy'
 alias acs='apt-cache search'
 alias acsh='apt-cache show'
+alias afs='apt-file search'
 alias upgr='sudo apt-get update && sudo apt-get -V upgrade'
 alias dpl='dpkg -l'
 alias dpL='dpkg -L'
+alias wd="cd ~/git/bunsenlabs/bunsen-utilities"
 
-export EDITOR='emacsclient -c'
+export EDITOR='emacsclient'
